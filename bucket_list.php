68cd263fc4eccaac9d7e4e29cbb1d92ab06d1d93
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-03-19 15:12:52 +0100

    lvl 1 opdr 5

--- bucket_list.php
+++ bucket_list.php
@@ -1,15 +1,20 @@
 <?php
 
-$aantal = readline("hoeveel activiteiten wilt u op uw bucket list");
-
-if(is_numeric($aantal)){
-
+$aantal = readline("hoeveel activiteiten wil je toevoegen?");
+$bucket = array();
+if(is_numeric($aantal)) {
+    for($i=1; $i <= $aantal; $i++) {
+  echo "welke activiteiten wil je toevoegen?";
+  $activiteit = readline();
+    array_push($bucket , $activiteit);
+    }
+echo 'In uw bucketlist staat:' ."\n";
+    for($j = 0; $j < $aantal; $j++) {
+        echo $bucket[$j] . "\n";
+    }
 }else{
-    exit("het is geen getal");
+    echo "dit is geen getal.";
 }
-
-for ($i = 1; $i <= $aantal; $i ++){
-   readline("voeg een activiteit toe"."\n");
-}
+?>
 
 
